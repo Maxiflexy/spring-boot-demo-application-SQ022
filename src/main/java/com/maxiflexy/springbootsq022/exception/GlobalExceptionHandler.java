96e0c17d3c4fc53baaea350da5282d88ec70edc9
Maxiflexy <onyemax247@gmail.com>
2024-06-19 11:51:28 +0100

    another commit

---
+++ src/main/java/com/maxiflexy/springbootsq022/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.maxiflexy.springbootsq022.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
